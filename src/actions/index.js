af5518fe0c56ac871e6d7b12851e79770f5e73df
PriyaG88 <priya.g71488@gmail.com>
2017-10-27 23:58:40 -0700

    Redirect user to employee list after successful login

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
 import { EMAIL_CHANGED,
   PASSWORD_CHANGED, LOGIN_USER_SUCCESS,
   LOGIN_USER_FAIL,
@@ -22,7 +23,7 @@ export const passwordChanged = (text) => {
 export const loginUser = ({ email, password }) => {
   return (dispatch) => {
     dispatch({ type: LOGIN_USER });
-    
+
     firebase.auth().signInWithEmailAndPassword(email, password)
       .then(user => loginUserSuccess(dispatch, user))
       .catch(() => {
@@ -44,4 +45,5 @@ const loginUserSuccess= (dispatch, user) => {
     type: LOGIN_USER_SUCCESS,
     payload: user
   });
+  Actions.employeeList();
 };
