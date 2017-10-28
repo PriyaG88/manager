2d1b5af8edc2d02e22a1e3775119e85ae530e6de
PriyaG88 <priya.g71488@gmail.com>
2017-10-28 01:07:13 -0700

    Add employee create scene

--- src/router.js
+++ src/router.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import { Router, Scene } from 'react-native-router-flux';
+import { Actions, Router, Scene } from 'react-native-router-flux';
 import LoginForm from './components/login_form';
 import EmployeeList from './components/employee_list';
+import EmployeeCreate from './components/employee_create';
 
 const RouterComponent = () => {
   return (
     <Router>
       <Scene key='root'>
-        <Scene key='login' component={LoginForm} title='Please Login'/>
-        <Scene key='employeeList' component={EmployeeList} title='Employees' />
+        <Scene key='auth'>
+          <Scene key='login' component={LoginForm} title='Please Login'/>
+        </Scene>
+        <Scene
+          onRight={() => Actions.employeeCreate()}
+          rightTitle='Add'
+          key='employeeList'
+          component={EmployeeList}
+          title='Employees' />
+
+        <Scene key='employeeCreate' component={EmployeeCreate} title='Create Employee' />
       </Scene>
     </Router>
   );
