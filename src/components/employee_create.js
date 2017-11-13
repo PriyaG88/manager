4991e9271eee701285375745cc8d2c620cfd130d
PriyaG88 <priya.g71488@gmail.com>
2017-11-13 14:43:58 -0800

    Add picker module to employee create

--- src/components/employee_create.js
+++ src/components/employee_create.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { Picker } from 'react-native';
+import { connect } from 'react-redux';
+import { employeeUpdate } from '../actions';
 import { Card, CardSection, Input, Button } from './common';
 
 class EmployeeCreate extends Component {
@@ -9,17 +11,45 @@ class EmployeeCreate extends Component {
         <CardSection>
           <Input
             label="Name"
-            placeholder="Sarah">
+            placeholder="Sarah"
+            value={this.props.name}
+            onChangeText={value => this.props.employeeUpdate({ props: 'name', value })}>
           </Input>
         </CardSection>
         <CardSection>
           <Input
             label="Phone"
-            placeholder="555-555-5555">
+            placeholder="555-555-5555"
+            value={this.props.phone}
+            onChangeText={value => this.props.employeeUpdate({ props: 'phone',
+            value })}>
           </Input>
         </CardSection>
         <CardSection>
-
+          <Picker
+            selectedValue={this.props.shift}>
+            <Picker.Item
+              label="Monday"
+              value="Monday"/>
+            <Picker.Item
+              label="Tuesday"
+              value="Tuesday"/>
+            <Picker.Item
+              label="Wednesday"
+              value="Wednesday"/>
+            <Picker.Item
+              label="Thursday"
+              value="Thursday"/>
+            <Picker.Item
+              label="Friday"
+              value="Friday"/>
+            <Picker.Item
+              label="Saturday"
+              value="Saturday"/>
+            <Picker.Item
+              label="Sunday"
+              value="Sunday"/>
+          </Picker>
         </CardSection>
         <CardSection>
           <Button>
@@ -31,4 +61,9 @@ class EmployeeCreate extends Component {
   }
 }
 
-export default EmployeeCreate;
+const mapStateToProps = state => {
+  const { name, phone, shift } = state.employeeForm;
+  return { name, phone, shift };
+};
+
+export default connect(mapStateToProps, { employeeUpdate })(EmployeeCreate);
