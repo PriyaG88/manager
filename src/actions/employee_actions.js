5874110642eefa1ecf34aaba0c457f9a5430d7a1
PriyaG88 <priya.g71488@gmail.com>
2017-11-14 14:51:15 -0800

    Set up action creator to fetch employees

--- src/actions/employee_actions.js
+++ src/actions/employee_actions.js
@@ -1,6 +1,8 @@
 import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
-import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE } from './types';
+import { EMPLOYEE_UPDATE,
+  EMPLOYEE_CREATE, EMPLOYEES_FETCH_SUCCESS }
+  from './types';
 
 export const employeeUpdate = ({ prop, value }) => {
   return {
@@ -18,6 +20,17 @@ export const employeeCreate = ({ name, phone, shift }) => {
     .then(() => {
       dispatch({ type: EMPLOYEE_CREATE });
       Actions.employeeList({ type: 'reset' });
+    });
+  };
+};
+
+export const employeeFetch = () => {
+  const { currentUser } = firebase.auth();
+  //snapshot is an object that describes the data - is not the data itself
+  return (dispatch) => {
+    firebase.database().ref(`/users/${currentUser.uid}/employees`)
+    .on('value', snapshot => {
+      dispatch({ type: EMPLOYEES_FETCH_SUCCESS, payload: snapshot.val() });
     });
   };
 };
